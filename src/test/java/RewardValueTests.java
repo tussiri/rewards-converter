f588b7e4996bbbde5d69a86e30e1c45a81d09271
Tuma <tussiri@gmail.com>
2023-07-06 09:49:18 -0500

    finished unite tests

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -20,11 +20,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedMiles=(int) (cashValue/.0035);
+        RewardValue rewardValue = new RewardValue(cashValue);
+        assertEquals(expectedMiles,rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue=10000;
+        double expectedCashValue=milesValue*.0035;
+        RewardValue rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
